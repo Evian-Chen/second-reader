66bd3841e84d93ddf2a1c6987c58f992d916a057
Evian-Chen <paramecium128@gmail.com>
2026-01-16 18:06:14 +0800

    feat: add clerk into frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,20 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { clerkPlugin } from '@clerk/vue'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
+
+app.use(clerkPlugin, { publishableKey: PUBLISHABLE_KEY })
 
 app.use(createPinia())
 app.use(router)
