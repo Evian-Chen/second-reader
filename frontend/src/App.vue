1ed5ace7b6409f1242164374b900650b342478c6
Evian-Chen <paramecium128@gmail.com>
2026-03-02 00:13:30 +0800

    feat: add clerk

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
+import ClerkView from './components/clerk/ClerkView.vue';
 </script>
 
 <template>
@@ -9,6 +10,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+
+      <ClerkView></ClerkView>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
