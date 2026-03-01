c27ea907fa2f879457d34cb92a2e066eac032afa
Evian-Chen <paramecium128@gmail.com>
2026-03-02 00:13:30 +0800

    feat: add clerk

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,21 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+import { clerkPlugin } from 'vue-clerk'
 
 import App from './App.vue'
 import router from './router'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+if (!PUBLISHABLE_KEY) throw new Error("Clerk Key not found.");
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(clerkPlugin, {
+  publishableKey: PUBLISHABLE_KEY
+})
 
 app.mount('#app')
