c6bc0a6973983da0b41d797a27ec7189866f8d3c
Evian-Chen <paramecium128@gmail.com>
2026-03-02 00:02:12 +0800

    feat: init vue porject

--- origin-vue/src/stores/index.ts
+++
@@ -1,7 +0,0 @@
-import { useAuthStore } from './auth'
-import { useBaseStore } from './base'
-import { useBookStore } from './book'
-
-export const useAuth = useAuthStore
-export const useBase = useBaseStore
-export const useBook = useBookStore
